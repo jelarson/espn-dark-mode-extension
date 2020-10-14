cbcaea6da9a4f773f6f6b04a4e31be1f1fb9114f
jelarson <jesslarson93@gmail.com>
2020-10-14 16:41:09 -0600

    dark and light mode functionality

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-function download() {
-  const filename = 'jobMatches'
-  chrome.storage.sync.get('jobInfo', (data) => {
-    const dataArr = [...data.jobInfo.list]
-    const file = new Blob([...dataArr], { type: 'text/plain' })
-    const a = document.createElement('a')
-    const url = URL.createObjectURL(file)
-    a.href = url
-    a.download = filename
-    document.body.appendChild(a)
-    a.click()
-    setTimeout(function () {
-      document.body.removeChild(a)
-      window.URL.revokeObjectURL(url)
-    }, 0)
-    console.log('I am downloading now!')
-  })
-}
-
 function dark() {
   chrome.storage.sync.set({
     running: true,
